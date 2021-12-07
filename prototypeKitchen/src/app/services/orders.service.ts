b29c617956bbf59e3dc0999edff77b8b6510e2de
Linderwolf <91448628+Linderwolf@users.noreply.github.com>
2021-12-06 21:48:14 -0500

    Now what

--- prototypeKitchen/src/app/services/orders.service.ts
+++ prototypeKitchen/src/app/services/orders.service.ts
@@ -44,9 +44,16 @@ export class OrdersService {
     },
     {
       orderID: 4,
-      user: {userID: 4, firstName: "Joan", lastName: "Clarke", address: "103 Gwynne Ave.", phone: 6476685864, email: "JoanDClarke@live.ca", password: "123321", isActive: true},
-      orderItems: OrderItems[1] = [{recipe: {recipeID: 5, recipeName: "Chiraishi", recipeEnglishName: "Chiraishi", recipeImage: "", recipeQuantity: 1, recipeDescription: "", recipePrice: 13.50, recipeIsAvailable: true, recipeIsPopular: true, recipeCalories: 1000}, orderQuantity: 1}],
+      user: {userID: 4, firstName: "Joan", lastName: "Clarke", address: "103 Gwynne Ave.", phone: 6476685864, email: "JoanDClarke@live.ca", password: "fj3ldgn4ol69fkws", isActive: true},
+      orderItems: OrderItems[1] = [{recipe: {recipeID: 4, recipeName: "Chiraishi", recipeEnglishName: "Chiraishi", recipeImage: "", recipeQuantity: 1, recipeDescription: "", recipePrice: 13.50, recipeIsAvailable: true, recipeIsPopular: true, recipeCalories: 1000}, orderQuantity: 1}],
       orderDate: "2021-11-30T19:22-05:00",
+      orderNotes: ""
+    },
+    {
+      orderID: 5,
+      user: {userID: 5, firstName: "Jordan", lastName: "Mihael", address: "100 Macdonelle Ave.", phone: 6473105436, email: "JM35d@yahoo.com", password: "password", isActive: true},
+      orderItems: OrderItems[1] = [{recipe: {recipeID: 6, recipeName: "Tamagoyaki", recipeEnglishName: "Fried Egg", recipeImage: "", recipeQuantity: 1, recipeDescription: "", recipePrice: 5.99, recipeIsAvailable: true, recipeIsPopular: true, recipeCalories: 1000}, orderQuantity: 1}],
+      orderDate: "2021-11-30T19:23-05:00",
       orderNotes: ""
     },
   ]
